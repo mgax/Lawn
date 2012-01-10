5a2b2296cd1af54765be89f4e639e1222af7a74c
Alex Morega <alex@grep.ro>
2012-01-10 21:59:52 +0200

    app factory; load configuration from pyfile

--- lawn.py
+++ lawn.py
@@ -1,13 +1,23 @@
+import os
 import flask
 
 
-app = flask.Flask(__name__)
+webpages = flask.Blueprint('webpages', __name__)
 
 
-@app.route('/')
+@webpages.route('/')
 def home():
     return flask.render_template('home.html')
 
 
+def create_app():
+    app = flask.Flask(__name__)
+    app.register_blueprint(webpages)
+    if 'APP_SETTINGS' in os.environ:
+        app.config.from_envvar('APP_SETTINGS')
+    return app
+
+
 if __name__ == '__main__':
+    app = create_app()
     app.run(debug=True)
