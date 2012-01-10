45eb460d1405d0277dff91603be9235a1a8c35e5
Alex Morega <alex@grep.ro>
2012-01-10 22:44:49 +0200

    initialize map

--- static/editor.js
+++ static/editor.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
 
-$('.map').text('map!');
+var wgs84 = new OpenLayers.Projection("EPSG:4326");
+function project_to_map(lonlat) {
+    var map_proj = map.getProjectionObject();
+    return lonlat.transform(wgs84, map_proj);
+}
+
+var map = new OpenLayers.Map('map');
+map.addLayer(new OpenLayers.Layer.OSM("OpenStreetMap"));
+map.setCenter(project_to_map(new OpenLayers.LonLat(26.082, 44.475)), 16);
 
 });
