38cb6fa597d8836d71e6921964000fcfc031250b
Alex Morega <alex@grep.ro>
2012-01-10 23:09:56 +0200

    "edit" button; pick rectangle to edit

--- static/editor.js
+++ static/editor.js
@@ -11,6 +11,30 @@ map.addLayer(new OpenLayers.Layer.OSM("OpenStreetMap"));
 map.setCenter(project_to_map(new OpenLayers.LonLat(26.082, 44.475)), 16);
 
 
-var menu_div = $('#menu').append('menu!');
+var menu_div = $('#menu');
+
+var edit_button = $('<a href="#" class="button">').text('edit');
+edit_button.appendTo(menu_div).click(function(evt) {
+    evt.preventDefault();
+    editing_context();
+});
+
+function editing_context() {
+    var layer = new OpenLayers.Layer.Vector('Edit', {});
+    map.addLayer(layer);
+
+    var edit_control = new OpenLayers.Control.ModifyFeature(layer, {
+        vertexRenderIntent: 'temporary',
+        mode: OpenLayers.Control.ModifyFeature.RESIZE |
+              OpenLayers.Control.ModifyFeature.RESHAPE |
+              OpenLayers.Control.ModifyFeature.DRAG
+    });
+    map.addControl(edit_control);
+    edit_control.activate();
+
+    var bounds = map.calculateBounds().scale(0.5);
+    var box = new OpenLayers.Feature.Vector(bounds.toGeometry());
+    layer.addFeatures([box]);
+}
 
 });
