79a35cfcc6ff1e3d1c140c7ef84ad1af15ee7edf
Alex Morega <alex@grep.ro>
2012-01-11 23:21:12 +0200

    refactor display_osm to be method of edit context

--- static/editor.js
+++ static/editor.js
@@ -37,38 +37,40 @@ L.editing_context = function(map) {
             console.log('ways: ' + $('osm > way', data).length);
             console.log('relations: ' + $('osm > relation', data).length);
             EC.map.addLayers([EC.node_layer, EC.way_layer]);
-            display_osm(data, EC.node_layer, EC.way_layer);
+            EC.display_osm(data);
             EC.edit_control = new OpenLayers.Control.ModifyFeature(EC.node_layer);
             EC.map.addControl(EC.edit_control);
             EC.edit_control.activate();
         });
     });
     L.message("Select area then click ", download_button);
+
+    EC.display_osm = function(osm_doc) {
+        EC.node_map = {};
+        $('osm > node', osm_doc).each(function() {
+            var node = $(this);
+            var lon = node.attr('lon'),
+                lat = node.attr('lat');
+            var point = new OpenLayers.Geometry.Point(lon, lat);
+            var feature = new OpenLayers.Feature.Vector(L.project_to_map(point));
+            EC.node_layer.addFeatures([feature]);
+            EC.node_map[node.attr('id')] = feature;
+        });
+
+        $('osm > way', osm_doc).each(function() {
+            var way = $(this);
+            var line_string = new OpenLayers.Geometry.LineString();
+            $('> nd', way).each(function() {
+                var node_feature = EC.node_map[$(this).attr('ref')];
+                line_string.addComponent(node_feature.geometry);
+            });
+            var feature = new OpenLayers.Feature.Vector(line_string);
+            EC.way_layer.addFeatures([feature]);
+        });
+    };
+
     return EC;
 };
 
-function display_osm(osm_doc, node_layer, way_layer) {
-    var node_map = {};
-    $('osm > node', osm_doc).each(function() {
-        var node = $(this);
-        var lon = node.attr('lon'),
-            lat = node.attr('lat');
-        var point = new OpenLayers.Geometry.Point(lon, lat);
-        var feature = new OpenLayers.Feature.Vector(L.project_to_map(point));
-        node_layer.addFeatures([feature]);
-        node_map[node.attr('id')] = feature;
-    });
-
-    $('osm > way', osm_doc).each(function() {
-        var way = $(this);
-        var line_string = new OpenLayers.Geometry.LineString();
-        $('> nd', way).each(function() {
-            var node_feature = node_map[$(this).attr('ref')];
-            line_string.addComponent(node_feature.geometry);
-        });
-        var feature = new OpenLayers.Feature.Vector(line_string);
-        way_layer.addFeatures([feature]);
-    });
-}
 
 })(window.L);
